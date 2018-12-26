d1c8b2fd6eea2a3a8e2021298e326375476b0d90
Yuanyuan <38313097+rainbow2019@users.noreply.github.com>
2018-12-26 09:40:17 +0800

    commit

--- src/main/java/jdk_dynamic_proxy/demo1/Test.java
+++ src/main/java/jdk_dynamic_proxy/demo1/Test.java
@@ -1,8 +1,6 @@
 package jdk_dynamic_proxy.demo1;
 
-import sun.awt.datatransfer.DataTransferer;
 import sun.misc.ProxyGenerator;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
